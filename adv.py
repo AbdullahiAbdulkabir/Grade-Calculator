334d7735454a134620be484a03255e77a868862a
Abdullahi Abdulkabir <33360580+AbdullahiAbdulkabir@users.noreply.github.com>
2018-07-04 13:53:31 +0100

    Add files via upload

---
+++ adv.py
@@ -0,0 +1,16 @@
+#x = int(input("enter a num "))
+
+#if(x > 2):
+#    if(x ==2):
+    #    print("equal to 2")
+ #   elif(x==3):
+  #      print("equal to 3")
+#else:
+ #   print("it is less than 2")
+
+x = str(input("name"))
+if (x=="Abdul"):
+    print("correct")
+else:
+    print("wrong")
+    
